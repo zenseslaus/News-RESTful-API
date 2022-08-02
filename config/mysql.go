a907d0d615e2ab51ecde1422219175242494c104
zenseslaus <zenseslaus@gmail.com>
2022-08-02 13:15:57 +0700

    Add Skeleton

---
+++ config/mysql.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDB() *gorm.DB {
+	dbUsername := os.Getenv("DB_Username")
+	dbPassword := os.Getenv("DB_Password")
+	dbPort := os.Getenv("DB_Port")
+	dbHost := os.Getenv("DB_Host")
+	dbName := os.Getenv("DB_Name")
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=UTC",
+		dbUsername,
+		dbPassword,
+		dbHost,
+		dbPort,
+		dbName)
+
+	db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	InitMigrate(db)
+	return db
+}
+
+func InitMigrate(db *gorm.DB) {
+
+}
